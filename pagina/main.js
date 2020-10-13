e3b5d6fd7f5cef2785a0e4bcb7ba822613f89b90
Lester-Alonzo <60448940+Lester-Alonzo@users.noreply.github.com>
2020-10-13 16:05:00 -0600

    Add files via upload

---
+++ pagina/main.js
@@ -0,0 +1,44 @@
+let slideUp = {
+    distance: '180%',
+    origin: 'bottom',
+    opacity: 0
+};
+ScrollReveal().reveal('.tarjetas', slideUp, { delay: 1000 });
+
+let ambur = document.querySelector('.lineas')
+let navegacion = document.querySelector('#navegacion')
+
+ambur.addEventListener('click', ()=>{
+navegacion.classList.toggle('show')
+})
+
+const Pespeci = document.querySelector('#espe')
+const desay = document.querySelector('#desak')
+const almu = document.querySelector('#almuer')
+const cena = document.querySelector('#cena')
+const refa = document.querySelector('#refac')
+const tipi = document.querySelector('#tipico')
+
+Pespeci.addEventListener('mouseover', ()=>{
+    
+})
+
+desay.addEventListener('mouseover', ()=>{
+    
+})
+
+almu.addEventListener('mouseover', ()=>{
+    
+})
+
+cena.addEventListener('mouseover', ()=>{
+    
+})
+
+refa.addEventListener('mouseover', ()=>{
+    
+})
+
+tipi.addEventListener('mouseover', ()=>{
+    
+})
